27e4c2272eefd51ad08f7c3eb85a5b5d98e9e1c2
Jniko <nikolaisenmail@gmail.com>
2022-07-18 07:59:04 +0200

    Images

--- src/Components/Projects/Main_Projects.js
+++ src/Components/Projects/Main_Projects.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { StyledLink } from '../Top_bar_files/Styled_Single_Files/Styled_Links'
 import CoolCat from "./CatApi/CoolCat.jpg"
 import Weather from "./Single_files/Weather.jpg"
+import Monster from "./MonsterRolodex/monster.jpg"
 import { StyledLinkProject } from './Single_files/StyledProjectLinks'
 
 const Content = styled.main`
@@ -20,7 +21,7 @@ export function MainProjects() {
     <Content>
         <StyledLinkProject to={"/CatFacts"} style={{backgroundImage: `url(${CoolCat})`}}>To CatFacts</StyledLinkProject>
         <StyledLinkProject to={"/WeatherAPI"} style={{backgroundImage: `url(${Weather})`}}>To WeatherAPI</StyledLinkProject>
-        <StyledLinkProject to={"/MonsterRolodex"} style={{backgroundImage: `url(${CoolCat})`}}>To MonsterRolodex API</StyledLinkProject>
+        <StyledLinkProject to={"/MonsterRolodex"} style={{backgroundImage: `url(${Monster})`}}>To MonsterRolodex API</StyledLinkProject>
         <StyledLinkProject to={"/CatFacts"} style={{backgroundImage: `url(${CoolCat})`}}>To CatFacts</StyledLinkProject>
         <StyledLinkProject to={"/CatFacts"} style={{backgroundImage: `url(${CoolCat})`}}>To CatFacts</StyledLinkProject>
         <StyledLinkProject to={"/CatFacts"} style={{backgroundImage: `url(${CoolCat})`}}>To CatFacts</StyledLinkProject>
